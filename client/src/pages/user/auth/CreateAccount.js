6b9e4694c5dd1e516b34ddd131f44f63fda30e53
Khaled Nached <khalednached@Khaleds-MacBook-Pro.local>
2022-07-12 12:51:47 -0400

    Added popup modal for errors, bug after closing it does not allow for modal to open up, figuring issue out

--- client/src/pages/user/auth/CreateAccount.js
+++ client/src/pages/user/auth/CreateAccount.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState, Fragment, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import axios from "axios";
 
-//import ImageLight from '../assets/img/create-account-office.jpeg'
-//import ImageDark from '../assets/img/create-account-office-dark.jpeg'
 import { GithubIcon, TwitterIcon } from '../../../icons'
 import { Input, Label, Button } from '@windmill/react-ui'
 import {useFormik} from "formik";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationIcon } from '@heroicons/react/outline'
 import * as Yup from 'yup';
 function CreateAccount() {
   const [success, setSuccess] = useState(null);
   const [errors, setErrors] = useState(null);
   const [loading, setLoading] = useState(null);
+  const [open, setOpen] = useState(true)
+  const cancelButtonRef = useRef(null)
   //const [flashMsg, setFlashMsg] = useState(null);
   const validationSchema =  Yup.object({
     fName: Yup.string().required('your first name is required').min(3, 'your first name must contain at least 3 characters'),
@@ -70,6 +72,7 @@ function CreateAccount() {
 
 
   return (
+    <>
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div className="flex flex-col overflow-y-auto md:flex-row">
@@ -102,7 +105,6 @@ function CreateAccount() {
                 Create account
               </h1> 
               {!errors && <p className="text-green-400 font-semibold">{success ? success : ""} </p>}
-               {!success && <p className="text-red-400 font-semibold">{errors ? errors : ""}</p>}
               <Label className="mt-4">
                 <Input className="mt-1" style={formik.touched.fName && formik.errors.fName ? {borderWidth: 2, borderColor: '#f71665', color: '#f71665'} : null}
                    placeholder="Enter your first name" name="fName" onChange={formik.handleChange}  value={formik.values.fName} onBlur={formik.handleBlur}/>
@@ -147,16 +149,6 @@ function CreateAccount() {
                     Create your account
                   </Button>
                 
-
-
-
-                  
-                
-
-             
-
-             
-
               <hr className="my-8" />
 
               <Button block layout="outline">
@@ -182,6 +174,76 @@ function CreateAccount() {
         </div>
       </div>
     </div>
+
+       {/* Modal Popup for errors */}
+   
+
+       <Transition.Root show={errors ? open : false} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-black text-gray-900">
+                      Signup Error 
+                      </Dialog.Title> 
+                      <div className="mt-2">
+                      {!success && <p className="text-red-400 font-semibold">{errors ? errors : ""}</p>}
+                      
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                 {/* <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button> */}
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Ok
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+    </>
   )
 }
 
--- client/src/pages/user/auth/Login.js
+++ client/src/pages/user/auth/Login.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import axios from "axios";
 
-//import ImageLight from '../assets/img/login-office.jpeg'
-//import ImageDark from '../assets/img/login-office-dark.jpeg'
 import { GithubIcon, TwitterIcon } from '../../../icons'
 import { Label, Input, Button } from '@windmill/react-ui'
 import {useFormik} from "formik";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationIcon } from '@heroicons/react/outline'
 import * as Yup from 'yup';
 
 function Login() {
+  // States
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+  const [open, setOpen] = useState(true)
+  const cancelButtonRef = useRef(null)
+
+  // Login onSubmit
   const onSubmit = async(values)=>{
     setError(null);
     const response = await axios
@@ -42,6 +47,7 @@ function Login() {
 
 
   return (
+    <>
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div className="flex flex-col overflow-y-auto md:flex-row">
@@ -65,7 +71,6 @@ function Login() {
           <form autoComplete='off' onSubmit={formik.handleSubmit}>
             <div className="w-full">
               <h1 className="mb-4 text-3xl font-black text-gray-700 dark:text-white">Login to your account</h1>
-              <p className="text-red-400 font-semibold">{error ? error : ""}</p>
               <p className="text-green-400 font-semibold">{success ? success : ''}</p>
 
               <Label>
@@ -118,6 +123,76 @@ function Login() {
         </div>
       </div>
     </div>
+
+    {/* Modal Popup for errors */}
+   
+
+    <Transition.Root show={error ? open : false} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-black text-gray-900">
+                      Login Error 
+                      </Dialog.Title> 
+                      <div className="mt-2">
+                      <p className="text-red-400 font-semibold">{error ? error : ""}</p>
+                      
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                 {/* <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button> */}
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Ok
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+    </>
   )
 }
 
